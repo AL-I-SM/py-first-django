9d09a4f721ea977c3d37337f178f98c71ca4a2f4
Alexandr-Semenishin <schl2kh@gmail.com>
2023-05-20 12:29:09 +0300

    Работающий функционал страницы журнала

---
+++ classbook/templatetags/filters.py
@@ -0,0 +1,35 @@
+from django.template.defaultfilters import register
+
+
+@register.filter()
+def by_pupil(query_obj, key):
+    """ """
+    return query_obj.filter(pupil=key)
+
+
+@register.filter()
+def by_date(query_obj, key):
+    """ """
+    return query_obj.filter(date=key)
+
+
+@register.filter()
+def str_score(query_obj):
+    """ """
+    if query_obj.count() == 1:
+        return query_obj.first().score
+    if query_obj.count() > 1:
+        return ", ".join([str(qo.score) for i, qo in enumerate(query_obj)])
+    return ""
+
+
+
+@register.filter()
+def id_score(query_obj):
+    """ """
+    if query_obj.count() == 1:
+        return query_obj.first().id
+    if query_obj.count() > 1:
+        return ", ".join([str(qo.id) for i, qo in enumerate(query_obj)])
+    return ""
+
--- classbook/views.py
+++ classbook/views.py
@@ -1,18 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Pupils, Days, Score
+from .models import Pupils, Days, Score, Disciplines, Teachers
 from .forms import SelectJournalForms
 from django.core import serializers
-import datetime
 
-# Create your views here.
+
 def index(request):
     return render(request, 'classbook/index.html')
 
 
 def journal_select(request):
-    return redirect('journal', teacher=1, discipline=1, current_class=1)
+    return redirect('journal', current_class=1, discipline=1, teacher=1)
 
 
 @login_required
@@ -26,17 +25,12 @@ def journal(request, **kwargs):
         scores = Score.objects.filter(pupil__current_class=current_class,
                                       discipline_id=discipline,
                                       teacher_id=teacher)
-        sc = {}
-        dy = {}
         pupils = Pupils.objects.filter(current_class_id=current_class)
         days = Days.objects.all()
-        for day in days:
-            sc.update([(day.id, {})])
-            sc[day.id].update([(s.pupil, s.score) for s in scores.filter(date=day.date)])
-        # todo придется тут раписать все по строкам, а там только готовые данные
         table = {'days': days, 'pupils': pupils,
                  # "form": scores_form,
-                 'journal': journal, 'scores': sc, "t": 2}
+                 'teacher': teacher, 'discipline': discipline,
+                 'journal': journal, 'scores': scores}
         return render(request, 'classbook/journal.html', table)
     else:
         return redirect('journal',
@@ -49,13 +43,20 @@ def journal(request, **kwargs):
 def score(request):
     for data, score in request.GET.items():
         if score:
-            new_score = Score()
+            print(len(data.split()))
+            if len(data.split()) == 4:
+                new_score = Score()
+            if len(data.split()) > 4:
+                new_score = Score.objects.get(id=data.split()[4])
+                if score == str(new_score.score):
+                    continue
             new_score.date = Days.objects.get(pk=data.split()[0]).date
             new_score.pupil = Pupils.objects.get(pk=data.split()[1])
             new_score.score = score
-            # new_score.teacher = None
-            # new_score.discipline = None
+            new_score.teacher = Teachers.objects.get(pk=data.split()[2])
+            new_score.discipline = Disciplines.objects.get(pk=data.split()[3])
             new_score.save()
-    return HttpResponse(request.GET.keys())
+    return redirect('journal', current_class=2, discipline=1, teacher=9)
+    # return HttpResponse(request.GET.keys())
 
 
