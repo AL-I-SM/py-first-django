1a955c112f92aeba15e1b3410ae11f7f3a9e995e
AL_I_SM <s.pr.cnc@gmail.com>
2025-12-24 11:50:46 +0300

    доделана регистрация пользователе и авторизация

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name="login"),
-    re_path(r'^logout/$', LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    re_path(r'^logout/$', LogoutView.as_view(next_page='users:login'), name="logout"),
     re_path(r'^register/$', views.register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,21 +1,29 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 def register(request):
     if request.method != 'POST':
-        form = UserCreationForm()
+
+        form = CustomUserCreationForm()
     else:
-        form = UserCreationForm(data=request.POST)
-        # TODO а если нет, то как обрабатывается ошибка?
+        form = CustomUserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
             authenticated_user = authenticate(username=new_user.username,
                                           password=request.POST['password1'])
             login(request, authenticated_user)
-        return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse('index'))
+        
     context = {'form': form}
     return render(request, 'users/register.html', context)
